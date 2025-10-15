2d1641b2fcdc1760a1774e5d9b3710afe2035f6d
ElikkDev <eljanfn-bpa202@code.edu.az>
2025-10-16 03:10:37 +0400

    First task

---
+++ Methods homework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+
+class Program
+{
+    static void Main()
+    {
+        bool result = isEven(5);
+        Console.WriteLine(result);
+        
+    }
+
+    static bool isEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
