07d117b8d5d0df99fc605615f3aac2db6a97c894
ElikkDev <eljanfn-bpa202@code.edu.az>
2025-10-16 12:02:26 +0400

    fifth task

--- Methods homework/Program.cs
+++ Methods homework/Program.cs
@@ -63,39 +63,73 @@ class Program
     #endregion
 
     #region fourth task
-    static void Main()
-    {
+    //static void Main()
+    //{
 
-        int answer = Factorial(5);
-        Console.WriteLine(answer);
+    //    int answer = Factorial(5);
+    //    Console.WriteLine(answer);
 
-    }
+    //}
 
-    static int Factorial(int number)
-    {
-       int number2 = 1;
-        if (number == 0)
-        {
-            return 1;
-        }
+    //static int Factorial(int number)
+    //{
+    //   int number2 = 1;
+    //    if (number == 0)
+    //    {
+    //        return 1;
+    //    }
 
-        else if (number < 0)
-        {
-            return -1;
-        }
+    //    else if (number < 0)
+    //    {
+    //        return -1;
+    //    }
 
-        else
-        {
-            for (int i = 1; i <= number; i++)
-            {
-                number2 *= i;
-            }
+    //    else
+    //    {
+    //        for (int i = 1; i <= number; i++)
+    //        {
+    //            number2 *= i;
+    //        }
 
-            return number2;
-        }
-    }
+    //        return number2;
+    //    }
+    //}
     #endregion
 
+    #region Fifth task
+
+    static void Main()
+    {
+        bool result = isPrime(12);
+        Console.WriteLine(result);
+
+    }
+
+    static bool isPrime(int number)
+    {
+        int count = 0;
+
+        for (int i  = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+    }
+
+
+    #endregion
 
 
 
